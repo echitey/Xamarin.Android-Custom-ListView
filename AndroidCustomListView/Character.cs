bb378a562cccd3cf92d1fad1169681ed96875fbe
Yawo Echitey <yawo@echitey.com>
2016-10-20 20:26:05 -0200

    Added Character class and Dummy data method

---
+++ AndroidCustomListView/Character.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace AndroidCustomListView
+{
+	public class Character
+	{
+		public string name { get; set; }
+		public int age { get; set; }
+		public int pictureId { get; set; }
+		public override string ToString()
+		{
+			return string.Format("[Character: name={0}, age={1}, pictureId={2}]", name, age, pictureId);
+		}
+	}
+}
--- AndroidCustomListView/MainActivity.cs
+++ AndroidCustomListView/MainActivity.cs
@@ -1,13 +1,16 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using System.Collections.Generic;
+using System;
 
 namespace AndroidCustomListView
 {
 	[Activity(Label = "AndroidCustomListView", MainLauncher = true, Icon = "@mipmap/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
+		
+		public List<Character> characters;
 
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
@@ -16,6 +19,23 @@ namespace AndroidCustomListView
 			// Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.Main);
 
+			characters = getCharacters();
+
+
+		}
+
+		private List<Character> getCharacters()
+		{
+			var _characters = new List<Character>();
+			_characters.Add(new Character { name = "Goomba", age = 12, pictureId = Resource.Drawable.Goomba});
+			_characters.Add(new Character { name = "Bowser", age = 23, pictureId = Resource.Drawable.PaperBowser });
+			_characters.Add(new Character { name = "Mario", age = 34, pictureId = Resource.Drawable.PaperMario });
+			_characters.Add(new Character { name = "Racoon Mario", age = 35, pictureId = Resource.Drawable.RacoonMario });
+			_characters.Add(new Character { name = "Shyguy Green", age = 11, pictureId = Resource.Drawable.ShyguyGreen });
+			_characters.Add(new Character { name = "MArio Baby", age = 12, pictureId = Resource.Drawable.SuperBabyMario });
+			_characters.Add(new Character { name = "Yoshi", age = 23, pictureId = Resource.Drawable.Yoshi });
+
+			return _characters;
 		}
 	}
 }
